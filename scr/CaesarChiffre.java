9a07ce94451cae95246fdc196b3b6207b9bf68c8
Thomas Witt <tom.89@gmx.at>
2020-02-12 10:06:31 +0100

    Cäsar chiffre with user-stupidity-correction.

--- scr/CaesarChiffre.java
+++ scr/CaesarChiffre.java
@@ -3,16 +3,25 @@ import java.util.Scanner;
 public class CaesarChiffre {
     public static void main(String[] args) {
         String userInput;
-        int characterJump = 6;
+        int characterJump;
+        System.out.println("Please enter chiffre value:");
         Scanner scanner = new Scanner(System.in);
-        userInput = scanner.next();
+        characterJump = scanner.nextInt();
+        if (characterJump < 0) {
+            characterJump = (characterJump % 26) * (-1);
+        } else {
+            characterJump = characterJump % 26;
+        }
+        System.out.println("Thank you!\nPlease enter message:");
+        Scanner scanner2 = new Scanner(System.in);
+        userInput = scanner2.next();
         char[] singleLetter = userInput.toCharArray();
         char[] changedLetter = new char[userInput.length()];
         for (int i = 0; i < userInput.length(); i++) {
             if (i < (26 - characterJump)) {
                 changedLetter[i] = (char) (singleLetter[i] + characterJump);
             } else {
-                changedLetter[i] = (char) (singleLetter[i] + characterJump);
+                changedLetter[i] = (char) (singleLetter[i] - 26 + characterJump);
             }
         }
         for (int i = 0; i < userInput.length(); i++) {
