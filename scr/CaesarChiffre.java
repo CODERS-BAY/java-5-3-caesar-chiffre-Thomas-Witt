15eb2b2359f6b6b483186402cd0f93b1ac51f187
Thomas Witt <tom.89@gmx.at>
2020-09-03 14:01:15 +0200

    let it chiffre at random or choose a number. you can even choose ridiculous high (like 1.000.000), or even negative numbers. Spaces won't be affected.

--- scr/CaesarChiffre.java
+++ scr/CaesarChiffre.java
@@ -1,31 +1,45 @@
+import java.util.Random;
 import java.util.Scanner;
 
 public class CaesarChiffre {
     public static void main(String[] args) {
-        String userInput;
-        int characterJump;
-        System.out.println("Please enter chiffre value:");
+        Random rand = new Random();
+
+        System.out.println("Please input the secret message:");
         Scanner scanner = new Scanner(System.in);
-        characterJump = scanner.nextInt();
-        if (characterJump < 0) {
-            characterJump = (characterJump % 26) + 26;
+        String message = scanner.nextLine();
+        System.out.println("Should it switch at random (press 1) or do you want to choose (press 2)");
+        int choice = scanner.nextInt();
+        int charjump;
+        if (choice == 2) {
+            System.out.println("How many places should it jump?");
+             charjump = scanner.nextInt();
         } else {
-            characterJump = characterJump % 26;
+            charjump = rand.nextInt(1000);
         }
-        System.out.println("Thank you!\nPlease enter message:");
-        Scanner scanner2 = new Scanner(System.in);
-        userInput = scanner2.next();
-        char[] singleLetter = userInput.toCharArray();
-        char[] changedLetter = new char[userInput.length()];
-        for (int i = 0; i < userInput.length(); i++) {
-            if (i < (26 - characterJump)) {
-                changedLetter[i] = (char) (singleLetter[i] + characterJump);
-            } else {
-                changedLetter[i] = (char) (singleLetter[i] - 26 + characterJump);
+        charjump = charjump % 26;
+        char[] characters = new char[message.length()];
+
+        for (int i = 0; i < message.length(); i++) {
+            characters[i] = message.charAt(i);              // Copies the message into an Char-Array
+        }
+        for (int i = 0; i < message.length(); i++) {
+            if ((int) characters[i] != 32) {
+                if ((int) characters[i] >= 97 && (int) characters[i] <= 122 && ((int) characters[i] + charjump) > 122) {
+                    characters[i] = (char) (characters[i] + charjump - 26);
+                } else if ((int) characters[i] >= 97 && (int) characters[i] <= 122 && ((int) characters[i] + charjump) < 97) {
+                    characters[i] = (char) (characters[i] + charjump + 26);
+                } else if ((int) characters[i] >= 65 && (int) characters[i] <= 90 && ((int) characters[i] + charjump) > 90) {
+                    characters[i] = (char) (characters[i] + charjump - 26);
+                } else if ((int) characters[i] >= 65 && (int) characters[i] <= 90 && ((int) characters[i] + charjump) < 65) {
+                    characters[i] = (char) (characters[i] + charjump + 26);
+                } else {
+                    characters[i] = (char) (characters[i] + charjump);
+                }
             }
         }
-        for (int i = 0; i < userInput.length(); i++) {
-            System.out.print(changedLetter[i]);
+        for (int i = 0; i < message.length(); i++) {
+            System.out.print(characters[i]);
         }
     }
 }
